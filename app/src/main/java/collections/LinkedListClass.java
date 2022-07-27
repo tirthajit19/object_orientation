a56cc0ca46f71dd92247e978224759f505960337
Tirthajit Das <tirthajitd@testvagrant.com>
2022-07-27 12:13:13 +0530

    Added different classes to Collections

---
+++ app/src/main/java/collections/LinkedListClass.java
@@ -0,0 +1,24 @@
+package collections;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LinkedListClass {
+    public static void main(String[] args) {
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.offer(12);
+        queue.offer(24);
+        queue.offer(36);
+
+        System.out.println(queue);
+
+        System.out.println(queue.poll());
+
+        System.out.println(queue);
+
+        System.out.println(queue.peek());
+
+    }
+}
